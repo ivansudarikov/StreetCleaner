b8f3e20504b1cf48e42bcb0cc77667118ea14813
ivansudarikov <iv.sudarikov@gmail.com>
2016-06-11 15:37:58 +0300

    Push initial commit

---
+++ street-cleaner-webapp/src/main/java/io/hackangel/street/cleaner/StreetCleanerApplication.java
@@ -0,0 +1,15 @@
+package io.hackangel.street.cleaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Ivan on 11.06.2016.
+ */
+@SpringBootApplication
+public class StreetCleanerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreetCleanerApplication.class, args);
+    }
+}
