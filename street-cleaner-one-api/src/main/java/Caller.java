9258b02790a99e4c35bba04a380eace31ac294e5
SevostyanovGerman <sevostyanovg.d@gmail.com>
2016-06-11 19:36:19 +0300

    Сделаны звонки,

---
+++ street-cleaner-one-api/src/main/java/Caller.java
@@ -0,0 +1,22 @@
+package main.java;
+
+import io.angelhack.rest.pojo.ResponseScenario;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class Caller {
+    public static void call(String number) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        StringBuilder startScenarios = new StringBuilder("https://api.voximplant.com/platform_api/StartScenarios/?account_id=" +
+                530063 + "&api_key=" +
+                "2bc17e3d-8bb9-4b94-bae7-a8b342e87e64" + "&rule_id=201263"
+                + "&script_custom_data=" + number);
+
+        ResponseEntity<ResponseScenario> responseScenario =
+                restTemplate.getForEntity(startScenarios.toString(), ResponseScenario.class);
+
+        System.out.println("Результат выполнения сценария:");
+        System.out.println(responseScenario);
+    }
+}
