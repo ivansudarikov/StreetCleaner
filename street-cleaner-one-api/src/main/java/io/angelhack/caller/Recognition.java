77cf1e9eecdfb995f248ff18ff8c4007fc9ec0dc
SevostyanovGerman <sevostyanovg.d@gmail.com>
2016-06-12 01:01:33 +0300

    Добавили передобавили

--- street-cleaner-one-api/src/main/java/io/angelhack/caller/Recognition.java
+++ street-cleaner-one-api/src/main/java/io/angelhack/caller/Recognition.java
@@ -10,13 +10,16 @@ public class Recognition {
         Ocr.setUp(); // one time setup
         Ocr ocr = new Ocr(); // create a new OCR engine
         ocr.startEngine("eng", Ocr.SPEED_FASTEST); // English
-        if (new File("test.jpg").exists()) {
-            String s = ocr.recognize(new File[]{new File("test.jpg")},
+        if (new File("test2.jpg").exists()) {
+            String s = ocr.recognize(new File[]{new File("test2.jpg")},
                     Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT); // PLAINTEXT | XML | PDF | RTF
             ocr.stopEngine();
 
+            System.out.println(s);
             int index = s.indexOf("8 *");
-            number.append(s.substring(index, index+15));
+            if (index != -1) {
+                number.append(s.substring(index, index + 15));
+            }
             return number.toString().replace("*", "9").replaceAll(" ", "");
         } else {
             System.out.println("false");
@@ -32,9 +35,26 @@ public class Recognition {
         String s = ocr.recognize(new File[]{new File("test.jpg")},
                 Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT); // PLAINTEXT | XML | PDF | RTF
         ocr.stopEngine();
+        System.out.println(s);
         int index = s.indexOf("8 *");
-        number.append(s.substring(index, index+15));
-        return number.toString().replace("*", "9").replaceAll(" ", "");
+        if (index == -1) {
+            index = s.indexOf("8*");
+        }
+        if (index == -1) {
+            index = s.indexOf("8-*");
+        }
+        if (index == -1) {
+            index = s.indexOf("+7*");
+        }
+        if (index == -1) {
+            index = s.indexOf("+7 *");
+        }
+        if (index == -1) {
+            index = s.indexOf("+7-*");
+        }
+        number = new StringBuilder(number.toString().replaceAll(" ", ""));
+        number.append(s.substring(index, index+11));
+        return number.toString().replace("*", "9");
 
     }
 
@@ -42,6 +62,7 @@ public class Recognition {
 
 
     public static void main(String[] args) {
-        Caller.call(recognize());
+        recognize();
+//        Caller.call(recognize());
     }
 }
--- street-cleaner-webapp/src/main/java/io/hackangel/street/cleaner/controllers/StreetCleanerController.java
+++ street-cleaner-webapp/src/main/java/io/hackangel/street/cleaner/controllers/StreetCleanerController.java
@@ -1,12 +1,14 @@
 package io.hackangel.street.cleaner.controllers;
 
-import io.angelhack.rest.pojo.Greeting;
 import io.angelhack.caller.Caller;
+import io.angelhack.rest.pojo.UserVox;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -19,15 +21,20 @@ public class StreetCleanerController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    @RequestMapping(value = "/getData", method = RequestMethod.GET)
+    public List<UserVox> test() {
+        UserVox userVox = new UserVox();
+        userVox.setAccount_email("test@test.ru");
+        userVox.setAccount_id(10L);
+        List<UserVox> userVoxList = new ArrayList<>();
+        userVoxList.add(userVox);
+        return userVoxList;
     }
 
     @RequestMapping(value = "/call", method = RequestMethod.GET)
     public void startCall(@RequestParam(value = "number", defaultValue = "+79118465234") String number) {
         Caller.call(number);
     }
+
 
 }
