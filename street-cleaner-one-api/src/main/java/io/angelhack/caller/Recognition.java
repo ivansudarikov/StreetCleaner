c65b1dd6ada6f37c6eac1e730d631d3d5d648a38
SevostyanovGerman <sevostyanovg.d@gmail.com>
2016-06-11 23:29:21 +0300

    Перегружен метод распознования

--- street-cleaner-one-api/src/main/java/io/angelhack/caller/Recognition.java
+++ street-cleaner-one-api/src/main/java/io/angelhack/caller/Recognition.java
@@ -14,13 +14,6 @@ public class Recognition {
             String s = ocr.recognize(new File[]{new File("test.jpg")},
                     Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT); // PLAINTEXT | XML | PDF | RTF
             ocr.stopEngine();
-//            for (int i = 0; i < s.length(); i++ ) {
-//                if ( (Character.isDigit(s.charAt(i)) && (number.length() < 11)) ) {
-//                    number.append(s.charAt(i));
-//                } else if ((s.charAt(i) == '*') && number.toString().startsWith("8")) {
-//                    number.append(9);
-//                }
-//            }
 
             int index = s.indexOf("8 *");
             number.append(s.substring(index, index+15));
@@ -30,6 +23,23 @@ public class Recognition {
             return "NOOOOOO";
         }
     }
+
+    public static String recognize(File file) {
+        StringBuilder number = new StringBuilder();
+        Ocr.setUp(); // one time setup
+        Ocr ocr = new Ocr(); // create a new OCR engine
+        ocr.startEngine("eng", Ocr.SPEED_FASTEST); // English
+        String s = ocr.recognize(new File[]{new File("test.jpg")},
+                Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT); // PLAINTEXT | XML | PDF | RTF
+        ocr.stopEngine();
+        int index = s.indexOf("8 *");
+        number.append(s.substring(index, index+15));
+        return number.toString().replace("*", "9").replaceAll(" ", "");
+
+    }
+
+
+
 
     public static void main(String[] args) {
         Caller.call(recognize());
